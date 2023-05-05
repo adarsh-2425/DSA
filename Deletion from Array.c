06acd0d534b506641863a98022bd76a066d8b846
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-05-05 13:43:42 +0530

    Update Deletion from Array.c

--- Deletion from Array.c
+++ Deletion from Array.c
@@ -1,5 +1,37 @@
 /******************************************************************************
 
+Array Operations - Deletion from Array in sorted array
+Time complexity  = O(n-p);
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[10] = {5 ,6, 1, 9, 2};
+    int n = 5;
+    int position = 4;
+    
+    int item = arr[position-1];
+    for(int i = position-1; i <= n-1; i++) {
+        arr[i] = arr[i+1];
+    }
+    n--;
+    
+    printf("\n%d deleted from position %d", item, position);
+    printf("\n");
+    printf("new array: ");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    
+
+
+    return 0;
+}
+/******************************************************************************
+
 Array Operations - Deletion from Array in unsorted array
 Time complexity  = O(1)
 
