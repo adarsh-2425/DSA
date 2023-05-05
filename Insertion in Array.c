e3e288ff680a13d1f7bac1106d89088aa78e0e12
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-05-05 13:12:43 +0530

    Update Insertion in Array.c

--- Insertion in Array.c
+++ Insertion in Array.c
@@ -1,6 +1,7 @@
 /******************************************************************************
 
 Array Operations - Traversal, Insertion in sorted array
+time complexity = O(n-p);
 
 *******************************************************************************/
 
@@ -15,6 +16,32 @@ int main()
     for(int i = n-1; i >= position-1; i--) {
         arr[i+1] = arr[i];
     }
+    arr[position-1] = value;
+    n++;
+    
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
+
+/******************************************************************************
+
+Array Operations - Traversal, Insertion in unsorted array
+time complexity = O(n);
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[10] = {5 ,6, 1, 9, 2};
+    int n = 5;
+    int position = 4, value = 3;
+    
+    arr[n] = arr[position-1];
     arr[position-1] = value;
     n++;
     
